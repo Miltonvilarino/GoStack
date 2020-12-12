64ed082653ddd4da7cf8623fd06d9b27fbef617a
Milton Vilarino Netto <miltonvilarino@gmail.com>
2020-12-12 16:24:29 -0300

    createUser feature.,

---
+++ backend-ts/src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import User from '../models/User';
+import { getRepository } from 'typeorm';
+import usersRouter from '../routes/users.routes';
+
+interface RequestDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password}: RequestDTO): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    })
+
+    if(checkUserExists) {
+      throw new Error('Email adress already used.')
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password
+    })
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+
+
+
+}
+
+export default CreateUserService;
